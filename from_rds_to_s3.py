1124908e67f64cae748db18854a6c576fc4c3935
Ubuntu <ubuntu@ip-172-31-11-207.me-south-1.compute.internal>
2024-04-28 09:05:31 +0000

    creating and uploading json files to bucket

--- from_rds_to_s3.py
+++ from_rds_to_s3.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import json
+import boto3
 import mysql.connector
 
 load_dotenv()
@@ -41,9 +42,40 @@ for r in record:
     num = num + 1
     dict_id_of_suadi_movies['show_id'].append({num: str(r)[1:-2]})
 
+# --- CREATE JSON FILE ---
 # CONVERT QUEIRY RESULT TO JSON FORMAT
 show_id_as_json = json.dumps(dict_id_of_suadi_movies, indent=2)
 # CREATE JSON FILE
 jsonFolder = "jsonFiles/"
 with open(f'{jsonFolder}saudi_movies_id_in_netflix.json', 'w') as jsonFile:
     jsonFile.write(show_id_as_json)
+    
+# --- CREATE AND UPLOAD TO AWS S3 BUCKET ---
+s3 = boto3.resource('s3')
+
+file_name = 'saudi_movies_id_in_netflix.json'
+bucket_name = os.getenv('S3_BUCKET')
+LocationConstraint = os.getenv('REGION')
+s3_folder = 'input'
+key = f'{s3_folder}/{file_name}'
+
+# CREATE BUCKET
+def create_s3_bucket(bucket_name, region):
+    try:
+        s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
+            'LocationConstraint': region,
+            })
+        print('s3 Bucket created successfully')
+    except Exception as error:
+        print(error)
+create_s3_bucket(bucket_name,LocationConstraint)
+
+# UPLOAD BUCKET
+def upload_to_s3_bucket(jsonFile, bucket_name, key):
+    try:
+        # key here is an organized way to put my (json file) inside a folder 
+        s3.meta.client.upload_file(jsonFile, bucket_name, key)
+        print("File Uploaded Succussfully")
+    except Exception as error:
+        print(error)
+upload_to_s3_bucket(jsonFolder + file_name, bucket_name, key)
