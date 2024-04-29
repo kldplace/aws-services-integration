7e6c920adf1ac66ff935b1f99940c12fce99a626
Ubuntu <ubuntu@ip-172-31-11-207.me-south-1.compute.internal>
2024-04-29 05:36:55 +0000

    handling json file

--- lambda-function/lambda_test.py
+++ lambda-function/lambda_test.py
@@ -34,8 +34,30 @@ info_of_suadi_movies = f"SELECT title, director, release_year FROM {database}.sh
 cursor.execute(info_of_suadi_movies)
 record = cursor.fetchall()
 
+# DICTIONARY FRO QUIERY RESULT
+file_name = 'saudi_movies_id_in_netflix.json'
+jsonFolder = "../jsonFiles/"
+
+with open(jsonFolder+file_name, "r") as file:
+    data = json.load(file)
+
+date = data["show_id"][0]
+print(date)
+dict_info_of_suadi_movies = {'suadi_movies_in_netflix': [] }
+num = 0
+for r in record:
+    num = num + 1
+    query_values = str(r)[1:-1]
+    query_values = query_values.split(',')
+    dict_info_of_suadi_movies['suadi_movies_in_netflix'].append({num: {'movie': query_values[0][1:-1], 'director': query_values[1][2:-1], 'release_year': query_values[2][1:]}})
+
+# --- CREATE JSON FILE ---
+# CONVERT QUEIRY RESULT TO JSON FORMAT
+show_info_as_json = json.dumps(dict_info_of_suadi_movies, indent=2)
+# print(show_info_as_json)
 # --- S3 BUCKET ---
 file_name = 'saudi_movies_id_in_netflix.json'
+jsonFolder = "../jsonFiles/"
 bucket_name = os.getenv('S3_BUCKET')
 s3_folder = 'input'
 key = f'{s3_folder}/{file_name}'
